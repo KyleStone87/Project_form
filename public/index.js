48915634386220b3dd4800e209883f601fa4eee3
Kyle Stone <kylestone87@gmail.com>
2017-12-10 14:34:18 -0800

    works, with modal. Needs validation

--- public/index.js
+++ public/index.js
@@ -1,13 +1,9 @@
 // Get the modal
-/*var modal = document.getElementById('myModal')
-
+var modal = document.getElementById('myModal')
 // Get the button that opens the modal
 var modalBtn = document.getElementById('modalBtn');
-
 // Get the <span> element that closes the modal
 var closeBtn = document.getElementsByClassName('modal-close')[0];
-
-
 // Listen for open click
 modalBtn.addEventListener('click', openModal)
 // Listen for close click
@@ -30,38 +26,38 @@ function outsideClick(e){
         modal.style.display = 'none'
         document.getElementById('index').style.filter = "blur(0px)"
     }
-}*/
+}
 $(document).ready(function(){
-
-    $('.delete').on('click', function(){
-        var id = $(this).closest('tr').attr('id')
-        var phone = $(this).find('.phone').text()
-        //var item = ''
-        $.ajax({
-            type: 'DELETE',
-            url: '/'+id,
-            //data:phone,
-            success: function(data){
-                location.reload()
-            }
+    
+        $('.delete').on('click', function(){
+            var id = $(this).closest('tr').attr('id')
+            var phone = $(this).find('.phone').text()
+            //var item = ''
+            $.ajax({
+                type: 'DELETE',
+                url: '/'+id,
+                //data:phone,
+                success: function(data){
+                    location.reload()
+                }
+            })
         })
-    })
-
-    $('.edit').on('click', function(){
-        var entry ={
-            name: $(this).closest('tr').find('.name').text(),
-            email: $(this).closest('tr').find('.email').text(),
-            phone: $(this).closest('tr').find('.phone').text()
-        }
-        
-        $.ajax({
-            type: 'POST',
-            url: '/',
-            data: entry,
-            success: function(data){
-                location.replace('/edit')
+    
+        $('.edit').on('click', function(){
+            var entry ={
+                name: $(this).closest('tr').find('.name').text(),
+                email: $(this).closest('tr').find('.email').text(),
+                phone: $(this).closest('tr').find('.phone').text()
             }
+            
+            $.ajax({
+                type: 'POST',
+                url: '/',
+                data: entry,
+                success: function(data){
+                    location.replace('/edit')
+                }
+            })
         })
+    
     })
-
-})
