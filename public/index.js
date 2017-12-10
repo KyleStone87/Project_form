5da9a3b4dce452606223a81a66a58eff192d49d9
Kyle Stone <kylestone87@gmail.com>
2017-12-10 14:01:44 -0800

    doesn't work, has modal and form validation

--- public/index.js
+++ public/index.js
@@ -1,4 +1,38 @@
+// Get the modal
+/*var modal = document.getElementById('myModal')
+
+// Get the button that opens the modal
+var modalBtn = document.getElementById('modalBtn');
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName('modal-close')[0];
+
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal)
+// Listen for close click
+closeBtn.addEventListener('click', closeModal)
+// Listen for outside click
+window.addEventListener('click', outsideClick)
+//function to open modal
+function openModal(){
+    modal.style.display = 'block'
+    document.getElementById('index').style.filter = "blur(5px)"
+}
+// function to close modal
+function closeModal(){
+    modal.style.display = 'none'
+    document.getElementById('index').style.filter = "blur(0px)"
+}
+//function to close modal if outside click
+function outsideClick(e){
+    if(e.target == modal){
+        modal.style.display = 'none'
+        document.getElementById('index').style.filter = "blur(0px)"
+    }
+}*/
 $(document).ready(function(){
+
     $('.delete').on('click', function(){
         var id = $(this).closest('tr').attr('id')
         var phone = $(this).find('.phone').text()
@@ -29,4 +63,5 @@ $(document).ready(function(){
             }
         })
     })
+
 })
